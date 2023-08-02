0f3d2190e74c7908e8004bc26b95395f8028b8fb
Jennifer Lopez <jenniferlopezh0@gmail.com>
2023-08-02 09:30:57 -0700

    fixed burger issue

--- js/script.js
+++ js/script.js
@@ -2,30 +2,22 @@ const getBurgerSection = document.getElementById("hamburger");
 const getNavList = document.getElementById("nav-list");
 const getLogo = document.getElementById("logo");
 
-const menu1 = document.getElementById("menu1");
-const menu2 = document.getElementById("menu2");
-const menu3 = document.getElementById("menu3");
-
-
-getBurgerSection.onclick = function () {
+getBurgerSection.addEventListener("click", function () {
     getBurgerSection.classList.toggle("active");
     getNavList.classList.toggle("show");
     getLogo.classList.toggle("hide");
-}
+})
 
-document.onclick = function (e) {
-    if (e.target.id == "menu1" || e.target.id == "menu2" || e.target.id == "menu3") {
+document.addEventListener("click", function (e) {
+    if (!getBurgerSection.contains(e.target) && !getNavList.contains(e.target)) {
         getBurgerSection.classList.remove("active");
         getNavList.classList.remove("show");
         getLogo.classList.remove("hide");
     }
 
-    if (e.target.id !== "menu1" &&
-        e.target.id !== "menu2" &&
-        e.target.id !== "menu3" &&
-        e.target.id !== "hamburger") {
+    if (getNavList.contains(e.target)) {
         getBurgerSection.classList.remove("active");
         getNavList.classList.remove("show");
         getLogo.classList.remove("hide");
     }
-}
+})
